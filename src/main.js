359cbf4c5e280288b221401ef9d885e19603ed22
kwok <kwok_fei@yeah.net>
2020-03-12 09:55:35 +0800

    图片上传

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/utils/setRem_api.js'
+import '@/assets/css_super/normalize.css'
 import '@/assets/css_super/public.css'
 
 Vue.use(ElementUI);
